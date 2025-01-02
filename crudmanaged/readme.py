bd924f0c868d20921a0c333089ca7ad108db5ef1
Jeeva Vinod <116909239+Je-eva@users.noreply.github.com>
2025-01-02 12:57:08 +0530

    Create readme.py

---
+++ crudmanaged/readme.py
@@ -0,0 +1,2 @@
+Here, instead of having one main.py having the routers, schemas, and the datas
+we separated each into its own code making the codes less messy and understand each codes functionality
